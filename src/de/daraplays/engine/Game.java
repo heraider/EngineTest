1bcb5fd19bbd0d57a0560aab0bf2a65bbe4c4fb4
heraider <heiko.bonnemann@gmx.de>
2013-03-01 17:14:53 +0100

    Gameloop und Berechnung Frames hinzugefügt

--- src/de/daraplays/engine/Game.java
+++ src/de/daraplays/engine/Game.java
@@ -1,6 +1,59 @@
 package de.daraplays.engine;
 
-public class Game {
-	// Test 
+import java.awt.Canvas;
 
+public class Game extends Canvas implements Runnable {
+
+	private static final long serialVersionUID = 1L;
+
+	public boolean running = false;
+
+	public Game() {
+
+	}
+
+	public void init() {
+
+	}
+
+	public synchronized void start() {
+		this.running = true;
+		new Thread(this).start();
+	}
+
+	public synchronized void stop() {
+		this.running = false;
+	}
+
+	@Override
+	public void run() {
+		long lastTimer = System.currentTimeMillis();
+		int frames = 0;
+
+		while (running) {
+			tick();
+			frames++;
+			render();
+
+			if (System.currentTimeMillis() - lastTimer >= 1000) {
+				lastTimer += 1000;
+				System.out.println(frames);
+				frames = 0;
+			}
+		}
+
+	}
+
+	public void tick() {
+
+	}
+
+	public void render() {
+
+	}
+
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.start();
+	}
 }
